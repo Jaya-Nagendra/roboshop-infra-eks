b3636234cd68c0ed00e8c33f7d9b0af2ce5d9222
Jaya-Nagendra <nagendra.44@outlook.com>
2026-04-23 23:25:41 +0530

    infra-k8s

---
+++ 70-acm/variables.tf
@@ -0,0 +1,17 @@
+variable "domain_name" {
+    type = string
+  default = "ljnag.space"
+}
+
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    type = string
+  default = "Z0264884DOSR5KP2H7MK"
+}
---
+++ 80-frontend-alb/variables.tf
@@ -0,0 +1,17 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    type = string
+  default = "Z0264884DOSR5KP2H7MK"
+}
+
+variable "domain_name" {
+    type = string
+  default = "ljnag.space"
+}
