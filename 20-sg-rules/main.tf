3de66bb6b40a15743d13e068644cdf92929b8f57
Jaya-Nagendra <nagendra.44@outlook.com>
2026-04-24 15:44:12 +0530

    k8s

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -126,3 +126,12 @@ resource "aws_security_group_rule" "eks_node_vpc_cidr" {
     cidr_blocks =["10.0.0.0/16"]
     security_group_id = local.eks_node_sg_id
 }
+
+resource "aws_security_group_rule" "eks_control_plane_eks_node" {
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
+  source_security_group_id = local.eks_node_sg_id
+  security_group_id        = local.eks_control_plane_sg_id
+}
